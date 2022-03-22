e729da64e405e312839a1b72575305aff29457f9
rulasg <rulasg@outlook.com>
2022-03-22 23:19:20 +0100

    x86 arch to manifest

--- src/DockerManifest.sh
+++ src/DockerManifest.sh
@@ -1,5 +1,5 @@
 # [Multi-arch build and images, the simple way](https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/)
 
-docker manifest create rulasg/echoserver:latest --amend rulasg/echoserver:amd64 --amend rulasg/echoserver:arm64v8
+docker manifest create rulasg/echoserver:latest --amend rulasg/echoserver:amd64 --amend rulasg/echoserver:arm64v8 --amend rulasg/echoserver:x64
 
 docker manifest push rulasg/echoserver:latest
